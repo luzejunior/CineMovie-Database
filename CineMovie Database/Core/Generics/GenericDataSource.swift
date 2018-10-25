2182bb7c4d30fcbded3ac45be715f2442568cf99
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2018-10-25 16:21:11 -0300

    Add tableview generic content

---
+++ CineMovie Database/Core/Generics/GenericDataSource.swift
@@ -0,0 +1,55 @@
+//
+//  GenericDataSource.swift
+//  CineMovie Database
+//
+//  Created by Luzenildo Junior on 25/10/18.
+//  Copyright © 2018 Luzenildo Junior. All rights reserved.
+//
+
+import UIKit
+
+final class GenericDataSource: NSObject, UITableViewDataSource {
+    
+    var items: [UITableViewModels] = []
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = items[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: model.representable.identifier, for: indexPath)
+        model.representable.update(view: cell)
+        return cell
+    }
+}
+
+protocol UITableViewModels {
+    var representable: UITableViewRepresentable { get }
+}
+
+protocol UITableViewRepresentable {
+    var identifier: String { get }
+    func update(view: UIView)
+}
+
+protocol UITableViewContent {
+    associatedtype Presenter
+    func load(presenter: Presenter)
+}
+
+final class UITableViewContentAssembler<T: UITableViewContent>: UITableViewRepresentable where T: UIView {
+    let presenter: T.Presenter
+    
+    init(presenter: T.Presenter) {
+        self.presenter = presenter
+    }
+    
+    func update(view: UIView) {
+        (view as? T)?.load(presenter: presenter)
+    }
+    
+    var identifier: String {
+        return String(describing: T.self)
+    }
+}
